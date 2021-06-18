73fb0d7d996885659a4781668b65275d9cd6f3ac
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-18 20:15:32 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+static FILE* open_flv(char *file_name){
+	FILE* fp = NULL;
+	
+	fp = fopen(file_name,"rb");
+	if(!fp){
+		return fp;
+	}
+	
+	fseek(fp,9,SEEK_SET);
+	fseek(fp,4,SEEK_CUR);
+	return fp;
+}
+
