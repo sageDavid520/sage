118b8b8757e9077025d2e3d4151e7c2e5b951c48
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-18 17:33:55 +0800

    Update push.c

--- push.c
+++ push.c
@@ -130,7 +130,7 @@ static int read_data(FILE *fp,RTMPPacket **packet){
 	if(tt == 9){
 		printf("====================\n");
 		for(i=0; i<5; ++i){
-		    printf("%c",*(body++));
+		    printf("%c\n",*(body++));
 		}
 		printf("====================\n");
 	}
