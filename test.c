258c2d328a2f4ef2bd9eb4a686dc8eee7dc22c2a
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-18 20:17:26 +0800

    Update test.c

--- test.c
+++ test.c
@@ -15,10 +15,12 @@ static FILE* open_flv(char *file_name){
 
 void pushlish_stream(){
 	char *flv = "./SampleVideo_1280x720_20mb.flv";
-	// need h264 + acc format flv video 
-	char *rtmpaddr = "rtmp://ai-livepush.zbitcloud.com/live/5_1624015825?txSecret=361f2ece71d6788b5c16154e67c37855&txTime=60ccae01";
 	FILE *fp = open_flv(flv);
-	RTMP *rtmp = connect_rtmp_server(rtmpaddr);
-	send_data(fp,rtmp);
+	
 	return ;
+}
+
+int main(){
+	pushlish_stream();
+	return 0;
 }
