f5012caedfb28cb8f91ad2115826c163d694d7e8
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-18 19:38:26 +0800

    Update push.c

--- push.c
+++ push.c
@@ -236,7 +236,7 @@ __ERROR:
 void pushlish_stream(){
 	char *flv = "./SampleVideo_1280x720_20mb.flv";
 	// need h264 + acc format flv video 
-	char *rtmpaddr = "rtmp://ai-livepush.zbitcloud.com/live/5_1624015771?txSecret=0812ae0fdcdde7f95cd51424e59e706f&txTime=60ccadcb";
+	char *rtmpaddr = "rtmp://ai-livepush.zbitcloud.com/live/5_1624015825?txSecret=361f2ece71d6788b5c16154e67c37855&txTime=60ccae01";
 	FILE *fp = open_flv(flv);
 	RTMP *rtmp = connect_rtmp_server(rtmpaddr);
 	send_data(fp,rtmp);
