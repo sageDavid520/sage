b711911a470b51692c13d8a302139c394614c31d
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-19 13:03:23 +0800

    Update test.c

--- test.c
+++ test.c
@@ -17,7 +17,7 @@ static FILE* open_flv(char *file_name){
 	fread(body,1,94577,fp);
 	// 4 字节长度
 	//int x = len[0] + len[1]*256 + len[2]*65536 + len[3]*16777216;
-	int x = body[5] + body[6]*256 + body[7]*65536 + body[8]*16777216;
+	int x = body[8] + body[7]*256 + body[6]*65536 + body[5]*16777216;
 	printf("%d\n",x);
 	// 1 字节类型
 	printf("%#x\n",body[9]);
