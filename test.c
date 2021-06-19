bbe7d92a2a2fd96469e066cd63534570fc1b692c
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-19 20:21:16 +0800

    Update test.c

--- test.c
+++ test.c
@@ -68,7 +68,7 @@ static FILE* open_flv(char *file_name){
 	body = (char*)malloc(94577);
 	dataTmpSize = fread(body,1,94577 ,fp);
 	
-	char* c2;
+	char* c2 = NULL;
 	StringToHex(body,c2);
 	printf("%#x,%#x,%#x,%#x",c2[0],c2[1],c2[2],c2[3]);
 	/*
