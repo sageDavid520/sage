85a1714ba005870711637189a7de50d4f0e6ab3d
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-19 12:56:29 +0800

    Update test.c

--- test.c
+++ test.c
@@ -20,7 +20,8 @@ static FILE* open_flv(char *file_name){
 	int tmp = atoi(len);
 	printf("%d\n",tmp);
 	// 4 字节长度
-	int x = len[0] + len[1]*256 + len[2]*65536 + len[3]*16777216;
+	//int x = len[0] + len[1]*256 + len[2]*65536 + len[3]*16777216;
+	int x = len[3] + len[2]*256 + len[1]*65536 + len[0]*16777216
 	printf("%d\n",x);
 	printf("%#x\n",len[0]);
 	printf("%#x\n",len[1]);
