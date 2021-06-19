7c5bc45d57f77175c5fb5567c224efda754d57ca
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-19 15:13:24 +0800

    Update test.c

--- test.c
+++ test.c
@@ -73,6 +73,16 @@ static FILE* open_flv(char *file_name){
 	dataTmpSize = fread(body,1,u322,fp);
 	printf("读取%d个字节主体数据\n",u322);
 	
+	// 下一个
+	read_u32(fp,&u322);
+	//printf("读取4个字节，长度为:%d\n",u322);// 这里有错误
+	
+	// 类型 1字节
+	read_u8(fp,&u82);
+	printf("读取1个字节，类型为:%d\n",u82);
+	
+	
+	
 	return fp;
 }
 
