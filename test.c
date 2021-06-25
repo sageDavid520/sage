67f2d1bc0784762cb3ba5f79131efb930ebf7828
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-25 13:56:42 +0800

    Update test.c

--- test.c
+++ test.c
@@ -84,8 +84,8 @@ static FILE* open_flv(char *file_name){
 
 
 	unsigned long int s[4];
-	char* str;
-	s = {
+	char str[4];
+	s[] = {
 		strtol(c0, &str, 16),
 		strtol(c1, &str, 16),
 		strtol(c2, &str, 16),
