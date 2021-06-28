8ac3d8c28b7d7c62b759060eb94b9e7b232ac35d
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-28 19:53:40 +0800

    Update test.c

--- test.c
+++ test.c
@@ -50,7 +50,12 @@ static FILE* open_flv(char *file_name){
 	body = (char*)malloc(94577);
 	dataTmpSize = fread(body,1,94577 ,fp);
 
-	printf("%#x\n",(0x000000FF & body[0] << 24) | 0x000000FF & body[1] << 16) | (0x000000FF & body[2]) << 8) | (0x000000FF & body[3]));
+	printf("%#x\n",(
+		(0x000000FF & body[0] << 24) | 
+		(0x000000FF & body[1] << 16) | 
+		(0x000000FF & body[2]) << 8) | 
+	        (0x000000FF & body[3])
+	      );
 	
 
 	//printf("%#x\n",((0x000000FF & body[2] << 8) | (0x000000FF & body[1] << 16) | (0x000000FF & body[0] << 24) | body[3]));
