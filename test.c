bbdb7d84017bfff4c33c1d3cc6aa40ed8a7d25c5
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-28 17:09:47 +0800

    Update test.c

--- test.c
+++ test.c
@@ -76,6 +76,10 @@ static FILE* open_flv(char *file_name){
 	q[2] |= i2;
 	q[3] |= i3;
 	
+	printf("%#x\n",i0);
+	printf("%#x\n",i1);
+	printf("%#x\n",i2);
+	printf("%#x\n",i3);
 	printf("%#x\n",(i0 & 0xFF | i1 & 0xFF00 | i2 & 0xff0000 | i3 & 0xff000000));
 	//*x = ((*q >> 24 & 0xFF)|(*q >> 8 & 0xFF00)|(*q << 8 & 0xFF0000)|(*q << 24 & 0xFF000000));
 	//*x = ((i0 >> 24 & 0xFF)|(i1 >> 8 & 0xFF00)|(i2 << 8 & 0xFF0000)|(i3 << 24 & 0xFF000000));
