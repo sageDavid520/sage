c6e51e13ab007ffa269a19010ecbb15aec2c146d
sageDavid520 <73159809+sageDavid520@users.noreply.github.com>
2021-06-28 17:42:21 +0800

    Update test.c

--- test.c
+++ test.c
@@ -80,7 +80,7 @@ static FILE* open_flv(char *file_name){
 	printf("%#x\n",i1);
 	printf("%#x\n",i2);
 	printf("%#x\n",i3);
-	printf("%#x\n",(i0 & 0xFF | i1 & 0xFF00 | i2 &  0xFF0000 | i3 &  0xFF000000));
+	printf("%#x\n",(i0 & 0xFF | i1 & 0xFF << 8 | i2 &  0xFF << 16 | i3 &  0xFF << 24));
 	//printf("%d\n",(i0 | i1 | i2 | i3));
 	//*x = ((*q >> 24 & 0xFF)|(*q >> 8 & 0xFF00)|(*q << 8 & 0xFF0000)|(*q << 24 & 0xFF000000));
 	//*x = ((i0 >> 24 & 0xFF)|(i1 >> 8 & 0xFF00)|(i2 << 8 & 0xFF0000)|(i3 << 24 & 0xFF000000));
